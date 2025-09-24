ba92451f5c5cef8c8e6840c3df1b5ec216d34174
Robroi Neo M. Dingal <robroineo2@gmail.com>
2025-09-24 22:01:51 +0800

    add core folder

--- public/index.php
+++ public/index.php
@@ -2,7 +2,10 @@
 
 const BASE_PATH = __DIR__ . '/../';
 
-require(BASE_PATH . 'functions.php');
-require base_path('Validator.php');
-require base_path('Database.php');
+require(BASE_PATH . 'core/functions.php');
+
+spl_autoload_register(function ($class) {
+    var_dump($class);
+    require base_path("core/" . $class . ".php");
+});
 require base_path('router.php');
